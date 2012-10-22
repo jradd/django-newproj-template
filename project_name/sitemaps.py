d52814a0804e6b9ae651dee733603d5b1a8413ef
Joseph Bergantine <jbergantine@gmail.com>
2012-10-22 12:59:32 -0600

    PEP8 Changes.

--- project_name/sitemaps.py
+++ project_name/sitemaps.py
@@ -3,11 +3,11 @@ from django.contrib.sitemaps import Sitemap
 
 class StaticSitemap(Sitemap):
     priority = 0.5
-    
+
     def items(self):
         return [
-            '/', 
+            '/',
         ]
-        
+
     def location(self, obj):
         return obj
