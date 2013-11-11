b21073f7b92c351f7f90efc9e92c74a63b03b8e3
Joseph Bergantine <jbergantine@gmail.com>
2013-11-11 15:43:10 -0700

    using BASE_DIR seems to then not require ../ to get up out of the settings dir

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -88,7 +88,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, '../static_media'),
+    os.path.join(BASE_DIR, 'static_media'),
 )
 
 # List of finder classes that know how to find static files in
@@ -114,7 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, '../templates'),
+    os.path.join(BASE_DIR, 'templates'),
 )
 
 # explicit specification necessary to include request context processor
